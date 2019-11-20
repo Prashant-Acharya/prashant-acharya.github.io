6771badd4fed033caf55cfea6fc339c55d3371b6
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2019-11-20 10:09:00 +0545

    test deployment 3

--- dist/app.5bd27738.js
+++ dist/app.5bd27738.js
@@ -36013,7 +36013,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39497" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40453" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
